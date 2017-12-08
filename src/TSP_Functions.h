6a333cbe1700ebda18f7b3a6686f6ca49664b490
jamesnyao <james.n.yao@gmail.com>
2017-12-08 13:59:18 -0500

    updated readme

--- src/TSP_Functions.h
+++ src/TSP_Functions.h
@@ -38,18 +38,29 @@ int best_first_search(int **adj, int v, int *shortest_path);
 **/
 int greedy_heuristic(int **adj, int v, int *shortest_path);
 
-/*
+/**
  * 	Tree node to hold path of nodes traversed in graph and cost
  * 	of path. If tree node is leaf, cost equals final cost. If tree
  * 	node is non-leaf, cost equals lower bound on all choices remaining.
- */
-class TreeNode {
-	public:
-		int cost;
-		int *path;
-		int length;
-		TreeNode();
-		~TreeNode() { delete[] path; }
+**/
+class TreeNode
+{
+public:
+
+	// lower bound, becomes exact cost of path if node is a leaf
+	int cost;
+	
+	// partial path
+	int *path;
+	
+	// length of partial path
+	int length;
+		
+	// Constructor
+	TreeNode();
+	
+	// Destructor
+	~TreeNode() { delete[] path; }
 };
 
 #endif
